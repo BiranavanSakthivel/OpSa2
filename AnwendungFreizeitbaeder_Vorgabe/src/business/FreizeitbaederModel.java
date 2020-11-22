bd19792a8baae464f82a01b6d16aeecf01154aad
Biranavan <Biranavan@192.168.0.87>
2020-11-22 19:52:11 +0100

    Verbessert

--- AnwendungFreizeitbaeder_Vorgabe/src/business/FreizeitbaederModel.java
+++ AnwendungFreizeitbaeder_Vorgabe/src/business/FreizeitbaederModel.java
@@ -11,13 +11,14 @@ public class FreizeitbaederModel {
 
 	public Freizeitbad freizeitbad;
 
+	/*
 	public void schreibeFreizeitbaederInCsvDatei() throws IOException {
 		// Werfen einer IOException
 		BufferedWriter aus = new BufferedWriter(new FileWriter("Freizeitbaeder.csv", true));
 		aus.write(this.getFreizeitbad().gibFreizeitbadZurueck(';'));
 		aus.close();
 	}
-
+	*/
 	public Freizeitbad getFreizeitbad() {
 		return freizeitbad;
 	}
